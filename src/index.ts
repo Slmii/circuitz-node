a3e49d0b21e7300f7281ad9bda8fe01e2f036574
Slmii <selami.c@sbytes-it.com>
2023-10-21 14:38:09 +0200

    root endpoint

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/', () => {
+	console.log('Hello World');
+});
 app.use('/icc', iccRoutes);
 app.use(errorHandler);
 
