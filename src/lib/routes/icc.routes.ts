38ddef57c84846fdf21c724c9b7026b7839c5074
Slmii <selami.c@sbytes-it.com>
2023-10-23 12:25:47 +0200

    add back ic0 call

--- src/lib/routes/icc.routes.ts
+++ src/lib/routes/icc.routes.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { ic } from 'ic0';
+import ic from 'ic0';
 import { validate } from 'lib/middlewares';
 import { iccSchema } from 'lib/schemas';
 import { ICC } from 'lib/types';
@@ -9,10 +9,10 @@ const iccRoutes = express.Router();
 iccRoutes.post('/', validate(iccSchema), async (req: Request<any, any, ICC>, res: Response, _next: NextFunction) => {
 	console.log('ICC Call', req.body, req.headers);
 
-	// const canister = ic(req.body.canisterId);
-	// const response = await canister.call(req.body.methodName, ...req.body.args);
+	const canister = ic(req.body.canisterId);
+	const response = await canister.call(req.body.methodName, ...req.body.args);
 
-	res.status(200).json(req.body);
+	res.status(200).json(response);
 });
 
 export { iccRoutes };
