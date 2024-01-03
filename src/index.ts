04f268fcd38280d5c6215df977a863f5be42e4d1
Slmii <selami.c@sbytes-it.com>
2024-01-04 00:55:05 +0100

    Commented out CORS middleware and added Access-Control-Allow-Origin header

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import { iccRoutes } from 'lib/routes';
 import { errorHandler } from 'lib/middlewares';
 import dotenv from 'dotenv';
@@ -10,13 +10,24 @@ const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-	cors({
-		origin: '*',
-		credentials: true,
-		optionsSuccessStatus: 200
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: '*',
+// 		credentials: true,
+// 		optionsSuccessStatus: 200
+// 	})
+// );
+
+app.use((_req, res, next) => {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Idempotency-Key, User-Agent, Host');
+
+	// Pass to next layer of middleware
+	next();
+});
 
 app.use('/icc', iccRoutes);
 app.use(errorHandler);
